80e031cadc0915d04d17403d6b16f116559bbde4
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-05-05 23:46:05 +0530

    added default auth

--- src/controller/auth/defaultController.js
+++ src/controller/auth/defaultController.js
@@ -4,14 +4,14 @@ const { getHashedPassword, checkPassword } = require("../../utils/bcryptManager"
 const { checkExistingUserDefault } = require("../../utils/datbaseUtils");
 const { getJwtToken, getRefreshToken } = require("../../utils/jwtManager");
 
-const CREATE_USER_QUERY = "INSERT INTO users(fullName,email,phone,password,authType,profileUrl) values (?,?,?,?,'password',?)"
+const CREATE_USER_QUERY = "INSERT INTO users(fullName,email,phone,authType,profileUrl) values (?,?,?,'password',?)"
 const DEFAULT_PROFILE_PLACEHOLDER_URL = "https://png.pngtree.com/png-clipart/20210129/ourmid/pngtree-default-male-avatar-png-image_2811083.jpg"
 
 const signUpWithPassword = async (req,res)=>{
     const fileName = req.imageName || DEFAULT_PROFILE_PLACEHOLDER_URL
     try{
 
-        const {fullName=null,email=null,password=null,phone=null} = req.body || {}
+        const {fullName=null,email=null,phone=null} = req.body || {}
         
         if(!fullName || !email || !password || !phone){
             return res.status(400).json({error:"Invalid Body"})
@@ -27,9 +27,11 @@ const signUpWithPassword = async (req,res)=>{
             return res.status(409).json({error:"Email Already Exist"})
         }
         
-        await pool.query(CREATE_USER_QUERY,[fullName,email,phone,await getHashedPassword(password),fileName])
+        await pool.query(CREATE_USER_QUERY,[fullName,email,phone,fileName])
         
-        return res.status(201).json({message:"User Created",jwt:getJwtToken({email:email,role:'user'}),refreshToken:getRefreshToken({email:email,type:'refresh'})})
+        return res.status(201).json({message:"User Created",jwt:getJwtToken({email:email,role:'user'}),refreshToken:getRefreshToken({user:{
+            email:email,fullName:fullName,phone:phone,profileUrl:fileName
+        },type:'refresh'})})
 
         
     }
